3db6e2b1055543eb4ee59d336c728ed4158a44eb
DraganovTs <cvetomir_draganov@abv.bg>
2024-07-10 18:58:11 +0300

    implement CRUD operations

--- accommodation-service/src/main/java/com/trip/advisor/accommodation/service/mapper/AccommodationMapper.java
+++ accommodation-service/src/main/java/com/trip/advisor/accommodation/service/mapper/AccommodationMapper.java
@@ -1,4 +1,29 @@
 package com.trip.advisor.accommodation.service.mapper;
 
+import com.trip.advisor.accommodation.service.model.dto.AddressDTO;
+import com.trip.advisor.accommodation.service.model.entity.Address;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AccommodationMapper {
+
+    public static AddressDTO mapToAddressDTO( AddressDTO addressDTO) {
+        return AddressDTO.builder()
+                .country(addressDTO.getCountry())
+                .state(addressDTO.getState())
+                .city(addressDTO.getCity())
+                .postalCode(addressDTO.getPostalCode())
+                .build();
+    }
+
+    public static Address mapToAddress(AddressDTO addressDTO){
+        return Address.builder()
+                .country(addressDTO.getCountry())
+                .state(addressDTO.getState())
+                .city(addressDTO.getCity())
+                .postalCode(addressDTO.getPostalCode())
+                .build();
+    }
+
+
 }
--- accommodation-service/src/main/java/com/trip/advisor/accommodation/service/model/dto/AddressDTO.java
+++ accommodation-service/src/main/java/com/trip/advisor/accommodation/service/model/dto/AddressDTO.java
@@ -2,7 +2,15 @@ package com.trip.advisor.accommodation.service.model.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AddressDTO {
     @Size(min = 3,max = 20)
     private String street;
