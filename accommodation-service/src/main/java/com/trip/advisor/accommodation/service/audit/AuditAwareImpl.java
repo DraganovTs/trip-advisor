10c309e657a2d1327755e49daa6f383ed1a25ec2
DraganovTs <cvetomir_draganov@abv.bg>
2024-07-22 20:40:57 +0300

    AuditAware

---
+++ accommodation-service/src/main/java/com/trip/advisor/accommodation/service/audit/AuditAwareImpl.java
@@ -0,0 +1,15 @@
+package com.trip.advisor.accommodation.service.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOMMODATION_MS");
+    }
+}
