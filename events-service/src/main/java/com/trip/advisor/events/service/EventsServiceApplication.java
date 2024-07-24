089331656d9fa9b408f5c36d765a9caefa6d0aab
DraganovTs <cvetomir_draganov@abv.bg>
2024-07-24 20:33:49 +0300

    audit and application.yml

--- events-service/src/main/java/com/trip/advisor/events/service/EventsServiceApplication.java
+++ events-service/src/main/java/com/trip/advisor/events/service/EventsServiceApplication.java
@@ -2,8 +2,10 @@ package com.trip.advisor.events.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class EventsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventsServiceApplication.class, args);
