adce0409e2b75f209e2f1ef6146bf1c3840a1ad2
DraganovTs <cvetomir_draganov@abv.bg>
2024-07-27 17:30:47 +0300

    RecommendationApplication

--- recommendation-service/src/main/java/com/trip/advisor/recommendation/service/controller/RecommendationController.java
+++ recommendation-service/src/main/java/com/trip/advisor/recommendation/service/controller/RecommendationController.java
@@ -1,5 +1,7 @@
 package com.trip.advisor.recommendation.service.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/recommendation")
 public class RecommendationController {
 
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createRecommendation(){
+        return ResponseEntity.ok().build();
+    }
 
 }
