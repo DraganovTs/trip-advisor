5c20b864376cbb61f4bfa715a9eea6394934cb4b
DraganovTs <cvetomir_draganov@abv.bg>
2024-12-02 21:09:20 +0200

    implement email service

--- messages-service/src/main/java/com/trip/advisor/message/service/service/impl/MessageServiceImpl.java
+++ messages-service/src/main/java/com/trip/advisor/message/service/service/impl/MessageServiceImpl.java
@@ -2,6 +2,10 @@ package com.trip.advisor.message.service.service.impl;
 
 import com.trip.advisor.message.service.model.dto.EmailDTO;
 import com.trip.advisor.message.service.service.MessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,6 +13,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageServiceImpl implements MessageService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MessageServiceImpl.class);
+    @Autowired
     private final JavaMailSender mailSender;
 
     public MessageServiceImpl(JavaMailSender mailSender) {
@@ -19,12 +25,16 @@ public class MessageServiceImpl implements MessageService {
     public void sendEmail(EmailDTO emailDTO) {
 
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("");
+        simpleMailMessage.setFrom("model3dservice@gmail.com");
         simpleMailMessage.setTo(emailDTO.getTo());
         simpleMailMessage.setSubject(emailDTO.getSubject());
         simpleMailMessage.setText(emailDTO.getMessage());
 
-        mailSender.send(simpleMailMessage);
+        try {
+            mailSender.send(simpleMailMessage);
+        } catch (MailException e) {
+            logger.error(e.getLocalizedMessage(),e);
+        }
     }
 
     @Override
